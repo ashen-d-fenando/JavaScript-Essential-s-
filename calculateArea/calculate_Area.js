e6ac6b8e256b44c41980a82ef8a119b59bd6da33
Ashen Dulara <fdo.ashen.d@gmail.com>
2025-02-24 04:12:42 -0500

    Added Practical Task

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,22 @@ function calculateArea(){
  let area = lenght * width;
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+
+
+//Calculate total amount 
+
+let  grocery1;  
+let grocery2;
+let grocery3; 
+
+function calTotal(){
+    grocery1 = parseFloat( document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = grocery1 + grocery2 + grocery3;
+    console.log(total);
+
+    document.getElementById("total").innerHTML = `The area of the rectangle is: ${total}`;
+}
