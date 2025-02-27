3cebafaac188b4a20d1c31b2c3aa19d330622466
Ashen Dulara <fdo.ashen.d@gmail.com>
2025-02-27 12:09:37 -0500

    Updated practical Task

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -28,7 +28,9 @@ function showBooks(){
     <p><strong>Book Name: </strong>${book.name}</p>
     <p><strong>Author Name:</strong> ${book.authorName}</p>
     <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-    <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>` );
+    <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+    <button onclick=(deleteBook(${index}))>Delete</button>` );
+    
 
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -38,4 +40,10 @@ function clearInputs(){
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deleteBook(index){
+    books.splice(index,1);
+    showBooks();
+    console.log("Deleted",books);
 }
